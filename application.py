5b2ab5cae34c0a89c486e0e759f1fcfc26f9cc27
ewokcillo <azotillo@gmail.com>
2012-12-07 18:43:27 +0100

    added bbdd wrappers

--- application.py
+++ application.py
@@ -6,6 +6,7 @@ from mako.lookup import TemplateLookup
 import tornado.ioloop
 import tornado.web
 from tornado import httpclient
+from db import Database
 
 root = os.path.dirname(__file__)
 template_root = os.path.join(root, 'templates')
@@ -36,12 +37,27 @@ class DefaultHandler(tornado.web.RequestHandler):
             return render_template(str(exception.code))
         return exceptions.html_error_template().render()
 
+    def return_json(self, data_dict):
+        self.set_header('Content-Type', 'application/json')
+        json_ = tornado.escape.json_encode(data_dict)
+        self.write(json_)
+        self.finish()
+
 class MainHandler(DefaultHandler):
     def get(self, filename):
          self.write(render_template(filename))
 
+class JsonHandler(DefaultHandler):
+    def get(self, collection):
+        data_instance = Database()
+        connection = data_instance.connect_mongo()
+        my_dict = data_instance.get_collection(connection, collection)
+        data_instance.disconnect_mongo(connection)
+        self.return_json(my_dict)
+
 application = tornado.web.Application([
-    (r'^/(.*)$', MainHandler),
+    (r'^/json/(?P<collection>[a-z]+)/$', JsonHandler),
+    (r'^/index.html$', MainHandler),
 ], debug=True, static_path=os.path.join(root, 'static'))
 
 if __name__ == '__main__':
