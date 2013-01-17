4e04b03eed7405c6a94d5c8f339d0619dc0bf7e2
ewokcillo <azotillo@gmail.com>
2013-01-17 13:28:27 +0100

    added required filters in voteresource

--- vote/api.py
+++ vote/api.py
@@ -1,6 +1,8 @@
 from tastypie.resources import ModelResource
 from tastypie import fields
+from tastypie.bundle import Bundle
 from vote.models import  Voting, Vote
+from tastypie.exceptions import InvalidFilterError
 
 class VotingResource(ModelResource):
     class Meta:
@@ -14,10 +16,31 @@ class VotingResource(ModelResource):
 class VoteResource(ModelResource):
     session = fields.IntegerField(attribute='voting__session', readonly=True)
     number = fields.IntegerField(attribute='voting__number', readonly=True)
+    
     class Meta:
-        queryset = Vote.objects.all()
+        resource_name = 'vote'
+        queryset = Vote.objects.filter()
         allowed_methods = ['get']
         filtering = {
                     "session": ('exact',),
                     "number": ('exact',),
         }
+
+    def build_filters(self, filters=None):
+        if filters is None:
+            raise InvalidFilterError("Session fnd number field are necessaries.")
+
+        if 'session' not in filters:
+            raise InvalidFilterError("Session field is necessary.")
+
+        if 'number' not in filters:
+            raise InvalidFilterError("Number field is necessary.")
+
+        orm_filters = super(VoteResource, self).build_filters(filters)
+                            
+        return orm_filters
+
+    def apply_filters(self, request, applicable_filters):
+        semi_filtered = super(VoteResource, self).apply_filters(request, applicable_filters)
+
+        return semi_filtered
