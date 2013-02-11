deb485bf8cf5277b745281b6a4a33c79c062202b
ewokcillo <azotillo@gmail.com>
2013-02-11 12:00:34 +0100

    added relation to parties in  member api

--- member/api.py
+++ member/api.py
@@ -3,12 +3,14 @@ from tastypie import fields
 from member.models import Member, MemberParty
 
 class MemberResource(ModelResource):
+    parties = fields.ToManyField('parliamentarygroup.api.PartyResource', 'memberparty_set', related_name='party')
     class Meta:
         queryset = Member.objects.all()
         allowed_methods = ['get']
 
 class MemberPartyResource(ModelResource):
     member = fields.ToOneField('members.api.MemberResource', 'member')
+    party = fields.ToOneField('members.api.PartyResource', 'party')
     class Meta:
         queryset = MemberParty.objects.all()
         allowed_methods = ['get']
