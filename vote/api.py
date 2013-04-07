53baecadf8ae5bd73d78a12fd20fc6226ab321ab
Javier Cordero <javier@georemindme.com>
2013-04-07 22:11:57 +0200

    Filter voting by session number

--- vote/api.py
+++ vote/api.py
@@ -5,18 +5,20 @@ from vote.models import  Voting, Vote, Session
 from tastypie.exceptions import InvalidFilterError
 from member.api import MemberResource
 
+
 class VoteResource(ModelResource):
-    session = fields.IntegerField(attribute='voting__session__session', readonly=True)
+    session = fields.IntegerField(
+        attribute='voting__session__session', readonly=True)
     number = fields.IntegerField(attribute='voting__number', readonly=True)
     member = fields.ToOneField(MemberResource, 'member')
-    
+
     class Meta:
         resource_name = 'vote'
         queryset = Vote.objects.filter()
         allowed_methods = ['get']
         filtering = {
-                    "session": ('exact',),
-                    "number": ('exact',),
+            "session": ('exact',),
+            "number": ('exact',),
         }
 
     def build_filters(self, filters=None):
@@ -30,13 +32,8 @@ class VoteResource(ModelResource):
             raise InvalidFilterError("Number field is necessary.")
 
         orm_filters = super(VoteResource, self).build_filters(filters)
-                            
         return orm_filters
 
-    def apply_filters(self, request, applicable_filters):
-        semi_filtered = super(VoteResource, self).apply_filters(request, applicable_filters)
-
-        return semi_filtered
 
 class VotingResource(ModelResource):
     class Meta:
@@ -44,5 +41,13 @@ class VotingResource(ModelResource):
         queryset = Voting.objects.all()
         allowed_methods = ['get']
         filtering = {
-                    "session": ('ALL_WITH_RELATIONS',),
+            "session": ALL_WITH_RELATIONS,
         }
+
+    def build_filters(self, filters=None):
+        orm_filters = super(VotingResource, self).build_filters(filters)
+        if 'session' in filters:
+            orm_filters.setdefault(
+                'session__session__in', filters.pop('session'))
+
+        return orm_filters
