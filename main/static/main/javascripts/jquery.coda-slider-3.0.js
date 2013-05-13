d697f4c4be19c32465378e3c793feea6477dfbc3
Andres Gutiérrez <andresgutgon@gmail.com>
2013-05-13 21:09:26 +0200

    Moved static files to main application folder

--- main/static/main/javascripts/jquery.coda-slider-3.0.js
+++ main/static/main/javascripts/jquery.coda-slider-3.0.js
@@ -28,7 +28,7 @@ if ( typeof Object.create !== 'function' ) {
 			$("body").removeClass("coda-slider-no-js");
 
 			//add preloader class (backwards compatible)
-			$('.coda-slider').prepend('<p class="loading">Loading...<br /><img src="/static/images/ajax-loader.gif" width="220" height="19" alt="loading..." /></p>');
+			$('.coda-slider').prepend('<p class="loading">Loading...<br /><img src="/static/main/images/ajax-loader.gif" width="220" height="19" alt="loading..." /></p>');
 
 			// Cache the element
 			self.elem = elem;
