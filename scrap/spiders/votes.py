3c00f5f677e81c3002398d077956965a169c3e1b
Javier Cordero <javier@georemindme.com>
2013-05-26 20:23:24 +0200

    Avoid raising exception when file is corrupt

--- scrap/spiders/votes.py
+++ scrap/spiders/votes.py
@@ -2,12 +2,15 @@
 
 import urlparse
 import re
+import logging
 
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import HtmlXPathSelector, XmlXPathSelector
 
 import scrap.items as items
+
+logger = logging.getLogger('scrapy')
 
 
 class VotesSpider(CrawlSpider):
@@ -24,7 +27,8 @@ class VotesSpider(CrawlSpider):
             callback='parse_vote'),
         Rule(
             SgmlLinkExtractor(
-                allow=['/wc/accesoHistoricoVotaciones&fechaSeleccionada=']),
+                allow=['/wc/accesoHistoricoVotaciones&fechaSeleccionada='],
+                unique=True),
             follow=True, callback='parse_session')]
 
     def parse_vote(self, response):
@@ -34,7 +38,12 @@ class VotesSpider(CrawlSpider):
             can't use '/votaciones/OpenData?sesion=\d*&completa=1&legislatura=10'
             as regex rule
         """
+
+        if not hasattr(response, 'body_as_unicode'):
+            logger.info('Cannot parse: %(u)s', u=response.url)
+            return
         x = XmlXPathSelector(response)
+
 
     def parse_session(self, response):
         """
