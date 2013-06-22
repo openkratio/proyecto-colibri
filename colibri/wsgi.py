b9465cda9c44bc2e56a5e608f2923048949f7d50
ewokcillo <azotillo@gmail.com>
2013-06-22 14:49:23 +0200

    quit print path from wsgi.py

--- colibri/wsgi.py
+++ colibri/wsgi.py
@@ -26,8 +26,6 @@ path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if path not in sys.path:
     sys.path.append(path)
 
-print path
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "colibri.settings")
 
 # This application object is used by any WSGI server configured to use this
