7313a4e15d791675bf7d8deb9b00fccb25221474
ewokcillo <azotillo@gmail.com>
2013-09-25 21:59:07 +0200

    added votings relation to initiative API resource

--- initiatives/api.py
+++ initiatives/api.py
@@ -10,15 +10,17 @@ class InitiativeResource(ColibriResource):
     authors = fields.ToManyField('member.api.MemberResource', 'author', full=True)
     authors_group = fields.ToManyField('parliamentarygroup.api.GroupManagerResource', 'author_group', full=True)
     commissions = fields.ToManyField('commission.api.CommissionManagerResource', 'author_group', full=True)
+    votings = fields.ToManyField('vote.api.VotingResource', 'votings')
 
     class Meta:
         resource_name = "initiative"
-        queryset = Initiative.objects.all().select_related('author', 'author_group', 'commissions')
+        queryset = Initiative.objects.all().select_related('author', 'author_group', 'commissions', 'votings')
         allowed_methods = ['get']
         filtering = {
                 "authors": ALL_WITH_RELATIONS,
                 "authors_group": ALL_WITH_RELATIONS,
                 "commissions": ALL_WITH_RELATIONS,
+                "votings": ALL_WITH_RELATIONS,
                 "calification_date": ALL,
                 "id": ALL,
                 "initiative_type": ALL,
